f268cdc8608e7759798296133444334ad9d46bc4
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2026-06-27 21:15:49 +0700

    fix: error & heading text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <h1>
+      <marquee behavior="" direction="">
+        Hello World :-)
+      </marquee>
+    </h1>
+  );
 }
 export default App;
